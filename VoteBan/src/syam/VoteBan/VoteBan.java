9fa7dca77257784dea376d6db8d7b498875c078a
syamn <admin@sakura-server.net>
2012-06-29 04:25:58 +0900

    Fixed onEnable NPE

--- VoteBan/src/syam/VoteBan/VoteBan.java
+++ VoteBan/src/syam/VoteBan/VoteBan.java
@@ -7,7 +7,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import syam.VoteBan.Commands.NoCommand;
 import syam.VoteBan.Commands.VoteCommand;
+import syam.VoteBan.Commands.YesCommand;
 import syam.VoteBan.Vote.Vote;
 import syam.VoteBan.VoteActions.BanHandler;
 
@@ -46,8 +48,8 @@ public class VoteBan extends JavaPlugin{
 
 		// コマンド登録
 		getServer().getPluginCommand("vote").setExecutor(new VoteCommand(this));
-		getServer().getPluginCommand("yes").setExecutor(new VoteCommand(this));
-		getServer().getPluginCommand("no").setExecutor(new VoteCommand(this));
+		getServer().getPluginCommand("yes").setExecutor(new YesCommand(this));
+		getServer().getPluginCommand("no").setExecutor(new NoCommand(this));
 		log.info(logPrefix+ "Initialized Command.");
 
 		// BANを行うプラグインの決定とハンドラ初期化
